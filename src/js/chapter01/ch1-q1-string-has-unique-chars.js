91eb0e9bfdbd49f75d617c4f12b26ff1e502e099
claudiuursica <claudiu.ursica@gmail.com>
2017-11-08 16:13:53 +0000

    Update ch1-q1-string-has-unique-chars.js

--- src/js/chapter01/ch1-q1-string-has-unique-chars.js
+++ src/js/chapter01/ch1-q1-string-has-unique-chars.js
@@ -3,7 +3,11 @@
  * 2. What if you cannot use additional data structures?
  */
 
+
 /**
+ * Time: O(N)
+ * Additional space: O(N)
+ *
  * @param {string} string - string to check
  * @returns {boolean} true if string contains only unique characters
  */
@@ -19,5 +23,27 @@ export default function hasUniqueCharacters(string) {
     charactersSet.add(currentChar);
   }
 
+  return true;
+}
+
+/**
+ * Time: O(N lg N)
+ * Additional space: O(1)
+ *
+ * @param {string} string - string to check
+ * @returns {boolean} true if string contains only unique characters
+ */
+export function hasUniqueCharactersSort(string) {
+  if (string.length > 256) {
+    return false
+  }
+  
+  const sortedStringAsArray = string.split('').sort();
+  for (let i = 1, len = sortedStringAsArray.length; i < len; i++) {
+    if (sortedStringAsArray[i - 1] === sortedStringAsArray[i]) {
+      return false;
+    }
+  }
+  
   return true;
 }
