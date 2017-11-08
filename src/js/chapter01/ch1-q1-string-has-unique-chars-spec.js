085d1367cc0695d1e80aff73cef09e6d04376b3a
claudiuursica <claudiu.ursica@gmail.com>
2017-11-08 16:15:17 +0000

    Update ch1-q1-string-has-unique-chars-spec.js

--- src/js/chapter01/ch1-q1-string-has-unique-chars-spec.js
+++ src/js/chapter01/ch1-q1-string-has-unique-chars-spec.js
@@ -1,4 +1,5 @@
 import hasUniqueCharacters from './ch1-q1-string-has-unique-chars';
+import hasUniqueCharactersSort from './ch1-q1-string-has-unique-chars';
 
 describe('Function: hasUniqueCharacters', () => {
   
@@ -8,6 +9,18 @@ describe('Function: hasUniqueCharacters', () => {
   
   it('should return false for non unique characters string', () => {
     expect(hasUniqueCharacters('absda')).toBe(false);
+  });
+  
+});
+
+describe('Function: hasUniqueCharactersSort', () => {
+  
+  it('should return true for unique characters string', () => {
+    expect(hasUniqueCharactersSort('abcd')).toBe(true);
+  });
+  
+  it('should return false for non unique characters string', () => {
+    expect(hasUniqueCharactersSort('absda')).toBe(false);
   });
   
 });
