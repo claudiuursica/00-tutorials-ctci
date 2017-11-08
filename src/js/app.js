3476dde9549953915affbe5d8b78243c1bf03c5b
claudiuursica <claudiu.ursica@gmail.com>
2017-11-08 14:13:21 +0000

    Create app.js

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import compareBinToHex from './chapter00/ch0-convert-from-base';
+import hasUniqueCharacters from './chapter01/ch1-q1-string-has-unique-chars';
+
+console.log("compareBinToHex() = ", compareBinToHex("111001011", "1CB"));
+console.log('hasUniqueCharacters() = ', hasUniqueCharacters('aabcdef'));
