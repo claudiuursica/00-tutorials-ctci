47728c03dbc67215a8abd5de8ce850755bf8ad3a
Claudiu Ursica <claudiu.ursica@gmail.com>
2017-11-09 22:12:49 +0000

    Added url-ify and test stubs

---
+++ src/js/chapter01/ch1-q3-urlify-spec.js
@@ -0,0 +1,8 @@
+import urlIfy from './ch1-q3-urlify';
+
+describe('Function: urlIfy', () => {
+  
+  it('should return true for unique characters string', () => {
+    expect(urlIfy('')).toBe('');
+  });
+});
---
+++ src/js/chapter01/ch1-q3-urlify.js
@@ -0,0 +1,17 @@
+/**
+ * Write a method to replace all spaces in a string with %20
+ *
+ * Note: In real life, one is better of using encodeURIComponent(), or String.replace()
+ */
+
+
+/**
+ * Time:
+ * Additional space:
+ *
+ * @param {string} string - target URL string
+ * @returns {string} true if string contains only unique characters
+ */
+export default function urlIfy(string) {
+  return string;
+}
