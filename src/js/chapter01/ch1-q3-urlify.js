87723aedd3602e8a941a18d3804cb4757b12e0b1
claudiuursica <claudiu.ursica@gmail.com>
2017-11-13 12:43:34 +0000

    Update ch1-q3-urlify.js

--- src/js/chapter01/ch1-q3-urlify.js
+++ src/js/chapter01/ch1-q3-urlify.js
@@ -5,14 +5,26 @@
  * Note: In real life, one is better of using encodeURIComponent(), or String.replace()
  */
 
-
 /**
- * Time:
- * Additional space:
  *
- * @param {string} string - target URL string
- * @returns {string} true if string contains only unique characters
+ * @param {string} string - target string
+ * @returns {string} string with spaces replaced by %2
  */
 export default function urlIfy(string) {
-  return string;
+  let output = '';
+  if (!string || string.length === 0) {
+    return output;
+  }
+  
+  let trimmedString = string.trim();
+  let len = trimmedString.length;
+  
+  let i = 0;
+  while (i < len) {
+    let currentChar = trimmedString.charAt(i);
+    output += (currentChar === ' ') ? '%20' : currentChar;
+    i++;
+  }
+  
+  return output;
 }
