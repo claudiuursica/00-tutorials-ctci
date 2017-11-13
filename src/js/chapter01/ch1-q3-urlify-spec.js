f87bf608ce341f8d7460fc2513945ab3a20c6ed5
claudiuursica <claudiu.ursica@gmail.com>
2017-11-13 12:45:23 +0000

    Update ch1-q3-urlify-spec.js

--- src/js/chapter01/ch1-q3-urlify-spec.js
+++ src/js/chapter01/ch1-q3-urlify-spec.js
@@ -2,7 +2,19 @@ import urlIfy from './ch1-q3-urlify';
 
 describe('Function: urlIfy', () => {
   
-  it('should return true for unique characters string', () => {
+  it('should return empty string for invalid input', () => {
+    expect(urlIfy(undefined)).toBe('');
+  });
+  
+  it('should return the empty string if the input is an empyt string', () => {
     expect(urlIfy('')).toBe('');
+  });
+  
+  it('should replace spaces with %20 in the string', () => {
+    expect(urlIfy('Test - Text')).toBe('Test%20-%20Text');
+  });
+  
+  it('should trim before replacing ', () => {
+    expect(urlIfy(' Test - Text ')).toBe('Test%20-%20Text');
   });
 });
