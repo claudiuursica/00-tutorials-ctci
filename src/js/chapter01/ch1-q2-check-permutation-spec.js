1c37a00ba66e2aa77d58ce279a2aa8948aab613c
claudiuursica <claudiu.ursica@gmail.com>
2017-11-13 11:50:31 +0000

    Update ch1-q2-check-permutation-spec.js

--- src/js/chapter01/ch1-q2-check-permutation-spec.js
+++ src/js/chapter01/ch1-q2-check-permutation-spec.js
@@ -1,5 +1,4 @@
 import checkPermutation from './ch1-q2-check-permutation';
-import checkPermutationSort from './ch1-q2-check-permutation';
 
 describe('Function: checkPermutation', () =>
 {
@@ -21,28 +20,5 @@ describe('Function: checkPermutation', () =>
   it('should return false if one string is not a permutaion of the other', () =>
   {
     expect(checkPermutation('abc', 'ba')).toBe(false);
-  });
-});
-
-describe('Function: checkPermutationSort', () =>
-{
-  it('should return false if one of the strings is missing', () =>
-  {
-    expect(checkPermutationSort('', undefined)).toBe(false);
-  });
-  
-  it('should return false strings have different lengths', () =>
-  {
-    expect(checkPermutationSort('ab', 'a')).toBe(false);
-  });
-  
-  it('should return true if strings have different lengths', () =>
-  {
-    expect(checkPermutationSort('ab', 'ba')).toBe(true);
-  });
-  
-  it('should return false if one string is not a permutaion of the other', () =>
-  {
-    expect(checkPermutationSort('abc', 'ba')).toBe(false);
   });
 });
