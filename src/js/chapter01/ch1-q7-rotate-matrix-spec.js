ab28c4317c28742f9580c5fd090cc801de919b12
claudiuursica <claudiu.ursica@gmail.com>
2017-11-27 16:32:27 +0000

    Update ch1-q7-rotate-matrix-spec.js

--- src/js/chapter01/ch1-q7-rotate-matrix-spec.js
+++ src/js/chapter01/ch1-q7-rotate-matrix-spec.js
@@ -2,31 +2,45 @@ import rotateMatrix from "./ch1-q7-rotate-matrix";
 
 describe('Function: rotateMatrix', () => {
   
+  it('should throw error', () => {
+    const test_Empty_matrix = [];
+    
+    expect(function() {rotateMatrix(test_Empty_matrix) }).toThrowError('Invalid matrix.');
+  
+    const rotated_2X3_matrix = [
+      [3, 1],
+      [4, 2],
+      [5, 6]
+    ];
+  
+    expect(function() {rotateMatrix(rotated_2X3_matrix) }).toThrowError('Invalid matrix.');
+  });
+  
   it('should return the rotated matrix', () => {
     const test_2X2_matrix = [
       [1, 2],
       [3, 4]
     ];
-  
+    
     const rotated_2X2_matrix = [
       [3, 1],
       [4, 2]
     ];
-
+    
     expect(rotateMatrix(test_2X2_matrix)).toEqual(rotated_2X2_matrix);
-
+    
     const test_3X3_matrix = [
       [1, 2, 3],
       [4, 5, 6],
       [7, 8, 9]
     ];
-  
+    
     const rotated_3X3_matrix = [
       [7, 4, 1],
       [8, 5, 2],
       [9, 6, 3]
     ];
-  
+    
     expect(rotateMatrix(test_3X3_matrix)).toEqual(rotated_3X3_matrix);
   });
 });
