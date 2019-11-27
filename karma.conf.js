b3fcc9f9ebfd1f2eee684162a852168125340c32
Claudiu Ursica <claudiu.ursica@gmail.com>
2019-11-27 20:34:23 +0000

    Update packages and the way test are ran

--- karma.conf.js
+++ karma.conf.js
@@ -1,72 +1,40 @@
-var webpackConfig = require('./webpack.config.js');
-
-// Karma configuration
-// Generated on Thu Nov 02 2017 16:30:45 GMT+0000 (GMT)
-
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
-    files: [
-      'src/js/**/*-spec.js'
-    ],
-
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      'src/js/**/*spec.js': ['webpack']
-    },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-  
-    webpack: webpackConfig,
-    
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: false,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
-}
+      basePath: './',
+      frameworks: ['jasmine'],
+      files: [
+          {pattern: './**/*-spec.js'},
+      ],
+      exclude: [],
+      preprocessors: {
+        'src/**/*.js': ['webpack'],
+      },
+      webpack: {
+          mode: 'development',
+      },
+      webpackMiddleware: {
+          noInfo: true,
+          stats: {
+              chunks: false,
+          },
+      },
+      reporters: ['progress'],
+      port: 9876,
+      colors: true,
+      logLevel: config.LOG_INFO,
+      autoWatch: true,
+      browsers: ['ChromeHeadless'],
+      singleRun: false,
+      customLaunchers: {
+          ChromeHeadless: {
+              base: 'Chrome',
+              flags: [
+                  '--no-sandbox',
+                  '--headless',
+                  '--disable-gpu',
+                  '--remote-debugging-port=9222',
+              ],
+          },
+      },
+  });
+};
